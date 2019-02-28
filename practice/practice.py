b1e99296cf612255472344a2dd6e2c907b270c07
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2019-02-27 18:22:09 -0600

    shell = false

--- practice/practice.py
+++ practice/practice.py
@@ -4,4 +4,4 @@ list_of_paths = os.listdir("../WikiCoref/WikiCoref-CoNLL")
 for i in range(len(list_of_paths)):
     list_of_paths[i] = "../WikiCoref/WikiCoref-CoNLL/" + list_of_paths[i]
 print(str(list_of_paths).replace("[","").replace("]","").replace("'","").replace(",",""))
-subprocess.call("split_train-dev-test.sh " + str(list_of_paths).replace("[","").replace("]","").replace("'","").replace(",",""), shell=True)
+subprocess.call("split_train-dev-test.sh " + str(list_of_paths).replace("[","").replace("]","").replace("'","").replace(",",""), shell=False)
