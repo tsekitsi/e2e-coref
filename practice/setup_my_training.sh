57f0f3404067f45506420834261028588f72da1b
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2019-02-28 00:11:01 -0600

    Finalizing

--- practice/setup_my_training.sh
+++
@@ -1,5 +0,0 @@
-python ../minimize.py
-python ../get_char_vocab.py
-
-python ../filter_embeddings.py ../glove.840B.300d.txt train.v4_gold_conll dev.v4_gold_conll
-python ../cache_elmo.py train.v4_gold_conll dev.v4_gold_conll
